5fa76ee2f28440e776f3169b3b538b87bf20b6ec
Basi6788 <chick786556@gmail.com>
2025-12-29 09:05:42 +0500

    Update Layout.tsx

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -11,9 +11,16 @@ const Layout: React.FC<LayoutProps> = ({ children, showFooter = true }) => {
   return (
     <div className="min-h-screen flex flex-col overflow-x-hidden">
       <Header />
-      <main className="flex-1 pt-16 md:pt-20 pb-20 md:pb-0 overflow-x-hidden">
+
+      {/* 
+        FIX:
+        - Removed bottom padding that was creating phantom scroll
+        - flex-1 already handles height correctly
+      */}
+      <main className="flex-1 pt-16 md:pt-20 overflow-x-hidden">
         {children}
       </main>
+
       {showFooter && <Footer />}
     </div>
   );
