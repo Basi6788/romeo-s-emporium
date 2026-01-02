eaf5ccf7eefb36cafcc69cbc83e1d5db3301ec85
Basi6788 <chick786556@gmail.com>
2026-01-02 16:10:40 +0500

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,4 +2,6 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
+// ⚠️ StrictMode hata diya hai taake Clerk Captcha crash na kare
 createRoot(document.getElementById("root")!).render(<App />);
+
