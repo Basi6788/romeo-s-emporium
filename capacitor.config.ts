d84a81048b6cfd99c05320c7f5473e10cf9f5fa6
Basit6788 <chick786556@gmail.com>
2026-01-11 15:44:06 +0500

    login

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,16 @@
-import type { CapacitorConfig } from '@capacitor/cli';
+import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.uchiha',
+  appId: 'com.romeo.uchiha',
   appName: 'MIRAE',
-  webDir: 'dist'
+  webDir: 'dist',
+  server: {
+    androidScheme: 'https',
+    // 👇 Asal Jadoo yahan hai: Apni Vercel website ka link yahan dalein
+    url: 'https://basit-shop1.vercel.app', 
+    cleartext: true
+  }
 };
 
 export default config;
+
